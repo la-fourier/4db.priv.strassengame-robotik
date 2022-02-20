1d852ac0724e43b8cfa2de4172eae73ba9dfdafd
la fourier <91893957+la-fourier@users.noreply.github.com>
2022-02-20 21:47:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ d=open("de.txt", "r"); dd=d.read(); exec(dd); d.close()
 
 
 # Definitions (2)
+dd=0
+def d():
+    pass
 def read_texte():
     d=open(voiceconfig["language"] +".txt", "r"); r=d.read(); exec(r); d.close()
 def buffer():
@@ -57,6 +60,7 @@ def output():
                 output_(text=texte[5][i], pos=(30, 5+23*i), text_color=Color.BLACK, background_color=Color.WHITE)
             while not ((Button.UP in ev3.buttons.pressed()) or (Button.DOWN in ev3.buttons.pressed())):
                 pass
+            d()
             if (Button.DOWN in ev3.buttons.pressed()):
                 emph=(emph+1)%len(texte[5]); wait(200); ev3.screen.clear(); continue
             elif (Button.UP in ev3.buttons.pressed()):
@@ -86,9 +90,110 @@ def output_(text="default", pos=(5, 60), text_color=Color.BLACK, background_colo
     ev3.screen.draw_text(x=pos[0], y=pos[1], text=text, text_color=text_color, background_color=background_color)
 
 
+# Leanders Teil
+def leaderboard_(change_=True, score=0, name=""):
+    if change_==True:
+        pass
+    else:
+        pass
+
+def leaderboard(change_, score=0, username=""):
+    score =  "1006"
+    username = " Jo"
+    if change_ == 1: # guckt, ob man aus dem Menue herkommt oder ein neuer Eintrag erstellt werden muss
+        #f=open("Leaderboard1.txt", "w")
+        #f.close()
+        file = open ("Leaderboard1.txt", "a")
+        file.write(score+";"+username+";")
+        file.close()
+
+    file = open ("Leaderboard1.txt", "r")
+    sortlist=[]
+    liste=[]
+    reader=file.read()
+    liste=reader.split(";")
+    for i in range(0, len(liste)-1):
+        if i%2!=0:  
+            sortlist.append([liste[i-1], liste[i]])
+
+    for i in range(len(sortlist)):
+        sortlist[i][0]=int(sortlist[i][int(0)])
+
+    for i in range(555):
+        for i in range(len(sortlist)-1):
+            if abs(sortlist[i][0]) < abs(sortlist[i+1][0]): # ordnet die Plaetze an
+                change=sortlist[i]
+                sortlist[i]=sortlist[i+1]
+                sortlist[i+1]=change
+    
+    for i in range(len(sortlist)):
+        sortlist[i][0]=str(sortlist[i][int(0)])
+    file.close()
+
+    #pos=1
+    #line=""
+    #pos_=0
+    #minpos=1
+    #position = 0 # macht die Position fuers scrollen zwischen Bildschirmen 
+    #reg = 1 # reguliert das Schreiben des Leaderbordes##
+
+    #while not Button.CENTER in ev3.buttons.pressed():
+    #    ev3.screen.clear()
+    #    for i in range(0, len(sortlist)-1): 
+    #        line=str(pos)+". "+"".join(sortlist[i])  
+    #        ev3.screen.draw_text(x=15, y=15+15*position, text=line)
+    #        position=position+1
+    #        pos=pos+1
+    #    #pos=1
+    #    #position=0        
+    #    #reg = position
+    #    while not ((Button.RIGHT in ev3.buttons.pressed()) or (Button.LEFT in ev3.buttons.pressed()) or (Button.UP in ev3.buttons.pressed()) or (Button.DOWN in ev3.buttons.pressed()) or (Button.CENTER in ev3.buttons.pressed())):
+    #        pass 
+    #    if (Button.RIGHT in ev3.buttons.pressed() or Button.DOWN in ev3.buttons.pressed()) and (minpos <= int(len(sortlist)/7)):
+    #        pos_ = pos_ - 7
+    #        minpos=minpos-1
+    #        position=pos_
+    #        wait(200)
+    #    elif (Button.LEFT in ev3.buttons.pressed() or Button.UP in ev3.buttons.pressed()) and (minpos != 0):
+    #        pos_ = pos_ + 7
+   #         minpos=minpos+1
+   #         position=pos_
+   #         wait(200)
+   # wait(500)
+
+    emph=0
+    position=0
+    rang=1
+    while not Button.CENTER in ev3.buttons.pressed():
+        ev3.screen.clear()
+        for i in range(emph*7, 7*(emph+1)):
+            try:
+                line=str(rang)+". "+"".join(sortlist[i])
+                ev3.screen.draw_text(x=15, y=15+15*position, text=line)
+                position=position+1
+            except:
+                pass
+        while not ((Button.RIGHT in ev3.buttons.pressed()) or (Button.LEFT in ev3.buttons.pressed()) or (Button.UP in ev3.buttons.pressed()) or (Button.DOWN in ev3.buttons.pressed()) or (Button.CENTER in ev3.buttons.pressed())):
+            pass
+        if (Button.RIGHT in ev3.buttons.pressed() or Button.DOWN in ev3.buttons.pressed()):
+            emph=emph+7
+            position=0
+            rang=rang+7
+            wait(200)
+        elif (Button.LEFT in ev3.buttons.pressed() or Button.UP in ev3.buttons.pressed()):
+            emph=emph-7
+            position=0
+            rang=rang+7
+            wait(200)
+    wait(500)
+
+
+
+
 # Begrüßung (3)
 ev3.screen.clear()
 showwait([texte[0][0]])
+d()
 buffer()
 
 
@@ -104,6 +209,7 @@ while not (Button.CENTER in ev3.buttons.pressed()):
             output_(text=texte[2][i], pos=(30, 15+23*i), text_color=Color.BLACK, background_color=Color.WHITE)
     while not ((Button.RIGHT in ev3.buttons.pressed()) or (Button.LEFT in ev3.buttons.pressed()) or (Button.UP in ev3.buttons.pressed()) or (Button.DOWN in ev3.buttons.pressed()) or (Button.CENTER in ev3.buttons.pressed())):
         pass
+    d()
     if (Button.DOWN in ev3.buttons.pressed()):
         emph=(emph+1)%len(texte[2])
         continue
@@ -122,7 +228,7 @@ while not (Button.CENTER in ev3.buttons.pressed()):
             continue
         elif emph==3:
             emph=0
-            # leanders ding
+            leaderboard(change_=1)
             continue
 
 
@@ -175,6 +281,7 @@ while run_==True:
 # Programmende (6)
 showwait(["Du hast verloren! Das Auto ist gegen ein Hindernis gedonnert..."])
 buffer()
+d()
 showwait(["Aber lass uns doch mal schauen, ob du vielleicht den Highscore geknackt hast!"])
 while not (Button.CENTER in ev3.buttons.pressed()):
     pass
